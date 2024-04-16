9a1a9fbaa1831530da23d909ec9d06298b251f4a
Maxxo0 <135011110+Maxxo0@users.noreply.github.com>
2024-04-16 10:05:35 +0200

    ShootSystem1

--- WizardPath/Assets/Wizard_Root/Scpits/Managers/GameManager.cs
+++ WizardPath/Assets/Wizard_Root/Scpits/Managers/GameManager.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("GameManager is null!");
+            }
+            return instance;
+        }
+    }
+
+    public enum ElementStatus { fire, water, air, earth, fw, fa, fe, wa, we, ae}
+
+    public ElementStatus actualElement = ElementStatus.fire;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ WizardPath/Assets/Wizard_Root/Scpits/PlayerScripts/ShootSystem.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSystem : MonoBehaviour
+{
+
+    #region General Variables
+    [Header("General References")]
+    [SerializeField] Camera fpsCam;
+    [SerializeField] Transform shootPoint;
+    [SerializeField] RaycastHit hit;
+    [SerializeField] LayerMask enemyLayer;
+
+    [Header("Elements")]
+    [SerializeField] GameObject fire;
+    [SerializeField] GameObject water;
+    [SerializeField] GameObject air;
+    [SerializeField] GameObject earth;
+    [SerializeField] GameObject fw;
+    [SerializeField] GameObject fa;
+    [SerializeField] GameObject fe;
+    [SerializeField] GameObject wa;
+    [SerializeField] GameObject we;
+    [SerializeField] GameObject ae;
+
+
+
+    [Header("Weapon Stats")]
+    public int damage;
+    public float range;
+    public float spread;
+    public float shootingCooldown;
+    public float timeBetweenShoots;
+    public float reloadTime;
+    public bool allowButtonHold;
+
+    [Header("Bullet Management")]
+    public int ammoSize;
+    public int BulletsPerTap;
+    [SerializeField] int bullletsLeft;
+    [SerializeField] int bullletsShot;
+
+    [Header("State Bools")]
+    [SerializeField] bool shooting;
+    [SerializeField] bool canShoot;
+    [SerializeField] bool reloading;
+
+    [Header("FeedBack & Graphics")]
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] GameObject hitGraphic;
+
+    #endregion
+
+
+    private void Awake()
+    {
+        bullletsLeft = ammoSize;
+        canShoot = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShootSyst();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ShootSyst()
+    {
+        switch (GameManager.Instance.actualElement) 
+        {
+
+            case GameManager.ElementStatus.fire:
+                if (canShoot && shooting && !reloading && bullletsLeft > 0)
+                {
+                    bullletsShot = BulletsPerTap;
+                    FireShoot();
+                }
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    void FireShoot()
+    {
+        canShoot = false;
+        Instantiate(fire, shootPoint.transform.position, shootPoint.transform.rotation);
+    }
+
+    void WaterShoot()
+    {
+        canShoot = false;
+        Instantiate(water, shootPoint.transform.position, shootPoint.transform.rotation);
+    }
+
+}
